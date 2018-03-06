fe0ba7dde844cc232bb555356fb5ff08a44b93b6
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-06 15:34:33 +0000

    change tests to use Matter

--- spec/CellSpec.js
+++ spec/CellSpec.js
@@ -2,17 +2,23 @@
 
 describe("Cell", function() {
   var cell;
+  var bodyCircle = Matter.Bodies.circle(400, 200, 40)
 
   beforeEach(function() {
-    cell = new Cell(400, 300);
+    cell = new Cell(bodyCircle);
   });
 
-  it("has an initial x position", function() {
-    expect(cell.x()).toEqual(400)
+  it("has a body", function() {
+    expect(cell.body()).toEqual(bodyCircle)
   });
 
-  it("has an initial y position", function() {
-    expect(cell.y()).toEqual(300)
-  });
+
+  // it("has an initial x position", function() {
+  //   expect(cell.x()).toEqual(400)
+  // });
+  //
+  // it("has an initial y position", function() {
+  //   expect(cell.y()).toEqual(300)
+  // });
 
 });
