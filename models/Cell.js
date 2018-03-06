c9a55dcad44f9c0d4dacfe7ed6d8bd06e384faaa
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-06 15:17:37 +0000

    red green REFACTOR x and y attributes
     co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- models/Cell.js
+++ models/Cell.js
@@ -2,9 +2,17 @@
 
 (function(exports) {
 
-  function Cell() {
-    this.x = 400;
-    this.y = 300;
+  function Cell(x, y) {
+    this._x = x;
+    this._y = y;
+  };
+
+  Cell.prototype.x = function(){
+    return this._x;
+  };
+
+  Cell.prototype.y = function(){
+    return this._y;
   };
 
 exports.Cell = Cell;
--- spec/CellSpec.js
+++ spec/CellSpec.js
@@ -4,12 +4,15 @@ describe("Cell", function() {
   var cell;
 
   beforeEach(function() {
-    cell = new Cell();
+    cell = new Cell(400, 300);
   });
 
-  it("has an initial position", function() {
-    expect(cell.x).toEqual(400)
-    expect(cell.y).toEqual(300)
+  it("has an initial x position", function() {
+    expect(cell.x()).toEqual(400)
+  });
+
+  it("has an initial y position", function() {
+    expect(cell.y()).toEqual(300)
   });
 
 });
