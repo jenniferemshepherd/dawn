e5564442e5a6e9181b8aec6dfc47e8bdc10e0d3f
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-09 11:58:16 +0000

    test for create and store
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- models/CellFactory.js
+++ models/CellFactory.js
@@ -1,1 +1,16 @@
 'use strict';
+
+(function(exports) {
+
+  function CellFactory() {
+  };
+
+  CellFactory.prototype.create = function(cellRepository) {
+    var cell = new Cell(Matter.Bodies.circle(150, 200, 10), new Gait());
+    cellRepository.add(cell);
+    return cell;
+  };
+
+  exports.CellFactory = CellFactory;
+
+})(this);
--- spec/CellFactorySpec.js
+++ spec/CellFactorySpec.js
@@ -1,1 +1,27 @@
 'use strict';
+
+describe("CellFactory", function() {
+  var cellFactory;
+  var cell;
+  var cellRepository = {
+    add: function() { return }
+  };
+
+  beforeEach(function() {
+    cellFactory = new CellFactory();
+  });
+
+  it("can create a cell", function() {
+    expect(cellFactory.create(cellRepository).body().type).toEqual('body')
+  });
+
+  it("stores it in the repository", function() {
+    spyOn(cellRepository, 'add')
+    cellFactory.create(cellRepository);
+    expect(cellRepository.add).toHaveBeenCalled();
+  });
+
+  it("can respond to an event", function() {
+
+  });
+});
