fac874ba82e0af41169df8549a47982a8963779f
rcvink <rrvink@gmail.com>
2018-03-10 19:44:53 +0000

    Pass instance of CellRepository to constructor

--- dawn.js
+++ dawn.js
@@ -4,21 +4,21 @@ var decoratedRenderer = new DecoratedRender();
 var eventController = new EventController();
 var simulation = new Simulation(decoratedEngine, decoratedRenderer);
 var cellRepository = new CellRepository();
-var cellFactory = new CellFactory(simulation);
+var cellFactory = new CellFactory(simulation, cellRepository);
 var animator = new Animator(cellRepository);
 
 // create render
 decoratedRenderer.createRender(decoratedEngine.matterEngine());
 
 // create some cells
-cellFactory.create(cellRepository)
-cellFactory.create(cellRepository)
-cellFactory.create(cellRepository)
+cellFactory.create();
+cellFactory.create();
+cellFactory.create();
 
 // register our listeners
 eventController.register(decoratedEngine.matterEngine(), 'afterUpdate', animator, cellRepository);
 eventController.register(decoratedEngine.matterEngine(), 'collisionStart', cellFactory, cellRepository);
 
 // run things
-simulation.setup()
+simulation.setup();
 simulation.run();
