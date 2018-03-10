98589707b199cfa240d9d5802f0e8c2a101e7ccf
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-10 15:33:12 +0000

    stitch in collisions
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- dawn.js
+++ dawn.js
@@ -18,6 +18,28 @@ cellFactory.create(cellRepository)
 // register our listeners
 eventController.register(decoratedEngine.matterEngine(), 'afterUpdate', animator, cellRepository);
 
+var fertile = true;
+var timeArray = [];
+
+Matter.Events.on(decoratedEngine.matterEngine(), 'collisionStart', function(event) {
+
+  var time = decoratedEngine.matterEngine().timing.timestamp;
+
+  if (event.pairs[0].bodyA.label !== 'Rectangle Body') {
+    if (time > timeArray[timeArray.length - 1] + 1000) {
+      fertile = true;
+    }
+
+    if (fertile) {
+      var cell = cellFactory.create(cellRepository);
+      simulation.addToWorld(cell);
+      fertile = false;
+      timeArray.push(time);
+    }
+  }
+
+});
+
 // run things
 simulation.setup()
 simulation.run();
