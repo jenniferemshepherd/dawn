b1cfea95565b2ec91a933c37284c41f7f9268708
rcvink <rrvink@gmail.com>
2018-03-13 16:12:24 +0000

    Add tests for cell fertility
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- spec/CellSpec.js
+++ spec/CellSpec.js
@@ -2,33 +2,39 @@
 
 describe("Cell", function() {
   var cell;
-  var bodyCircle = Matter.Bodies.circle(400, 200, 40);
-  var stoop = {};
-  var mockAge = {
-    value: function() { return }
+  var mockBody;
+  var mockGait;
+  var mockCurrentTime = 12500;
+  var mockAdolescentAge = {
+    value: function() { return 1000 }
   };
-  var mockCurrentTime = 7500;
+  var mockMiddleAge = {
+    value: function() { return 10000 }
+  };
+  var mockElderlyAge = {
+    value: function() { return 22000 }
+  };
 
   beforeEach(function() {
-    cell = new Cell(bodyCircle, stoop, mockAge);
+    cell = new Cell(mockBody, mockGait, mockAdolescentAge);
   });
 
   describe("initially,", function() {
 
     it("has a body", function() {
-      expect(cell.body()).toEqual(bodyCircle)
+      expect(cell.body()).toEqual(mockBody);
     });
 
     it("has a gait", function() {
-      expect(cell.gait()).toEqual(stoop)
+      expect(cell.gait()).toEqual(mockGait);
     });
 
     it("has an age", function() {
-      expect(cell.age()).toEqual(mockAge)
+      expect(cell.age()).toEqual(mockAdolescentAge);
     });
 
     it("is infertile", function() {
-      expect(cell.isFertile()).toEqual(false)
+      expect(cell.isFertile()).toEqual(false);
     });
 
   });
@@ -44,5 +50,31 @@ describe("Cell", function() {
     });
 
   });
+
+  describe("#isFertile()", function() {
+
+    it("returns true when cell has finished puberty", function() {
+      cell = new Cell(mockBody, mockGait, mockMiddleAge);
+      expect(cell.isFertile(mockCurrentTime)).toEqual(true);
+    });
+
+    it("returns false when cell has recently reproduced", function() {
+      cell = new Cell(mockBody, mockGait, mockMiddleAge);
+      cell.updateLastReproduction(mockCurrentTime);
+      expect(cell.isFertile(mockCurrentTime)).toEqual(false);
+    });
+
+    it("returns true when cell is adult and has not reproduced recently", function() {
+      cell = new Cell(mockBody, mockGait, mockMiddleAge);
+      cell.updateLastReproduction(mockCurrentTime);
+      expect(cell.isFertile(mockCurrentTime + 6000)).toEqual(true);
+    });
+
+    it("returns false when cell is elderly", function() {
+      cell = new Cell(mockBody, mockGait, mockElderlyAge);
+      expect(cell.isFertile(mockCurrentTime)).toEqual(false);
+    });
+
+  })
 
 });
