e5bbec1f3a7218a37cbd8677a1f53ba28f4b2e2d
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-14 16:51:40 +0000

    create cellFactory method for creating initial shapes
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- public/dawn-jquery.js
+++ public/dawn-jquery.js
@@ -1,51 +1,46 @@
 $( document ).ready(function() {
 
-  var cell = cellFactory.createCircle();
-  cellFactory.createSquare();
-  cellFactory.createEquilateralTriangle();
-  cellFactory.createTrapezoid();
-  Matter.Runner.run(runner, decoratedEngine.matterEngine());
+  cellFactory.createInitialShapes();
+  Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
   simulation.setup();
   simulation.run();
 
   $( "#start" ).click(function() {
-
-    var cell = cellFactory.createCircle();
-    cellFactory.createSquare();
-    cellFactory.createEquilateralTriangle();
-    cellFactory.createTrapezoid();
-    Matter.Runner.run(runner, decoratedEngine.matterEngine());
-    simulation.setup();
-    simulation.run();
+    if (decoratedEngine.matterEngine().timing.timestamp = 0) {
+      var cell = cellFactory.createCircle();
+      cellFactory.createSquare();
+      cellFactory.createEquilateralTriangle();
+      cellFactory.createTrapezoid();
+      Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
+      simulation.setup();
+      simulation.run();
+    };
     });
 
   $( "#stop" ).click(function() {
-    Matter.Runner.stop(runner);
+    Matter.Runner.stop(runner.matterRunner());
     Matter.Engine.clear(decoratedEngine.matterEngine());
     decoratedEngine.matterEngine().timing.timestamp = 0;
     simulation.world().bodies = [];
     });
 
   $( "#pause" ).click(function() {
-    Matter.Runner.stop(runner);
+    Matter.Runner.stop(runner.matterRunner());
     });
 
   $( "#unpause" ).click(function() {
-    Matter.Runner.run(runner, decoratedEngine.matterEngine());
+    Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
     });
 
   $('#refresh').click(function() {
-    Matter.Runner.stop(runner);
+    Matter.Runner.stop(runner.matterRunner());
     Matter.Engine.clear(decoratedEngine.matterEngine());
     decoratedEngine.matterEngine().timing.timestamp = 0;
-    simulation.world().bodies = []
-    var cell = cellFactory.createCircle();
-    cellFactory.createSquare();
-    cellFactory.createEquilateralTriangle();
-    Matter.Runner.run(runner, decoratedEngine.matterEngine());
+    simulation.world().bodies = [];
+    cellFactory.createInitialShapes();
+    Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
     simulation.setup();
     simulation.run();
-    // location.reload();
   });
 
   $('#wireframe').click(function() {
