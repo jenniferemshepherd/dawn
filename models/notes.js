8fed518596f4ab0ad7e4dabffa05f6d0f7a6a549
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 17:53:20 +0000

    add notes array functions

--- models/notes.js
+++ models/notes.js
@@ -6,6 +6,10 @@
      this._noteArray = [830.609, 932.328, 1046.50, 1108.73, 1244.51, 1396.91, 1567.98, 1661.22]
    };
 
+   Notes.prototype.array = function() {
+     return this._noteArray;
+  };
+
 exports.Notes = Notes;
 
 })(this);
