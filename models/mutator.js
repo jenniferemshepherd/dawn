ae6f597fe3a0764999b04e310e53490853ea1e14
Ellie <elliewem@gmail.com>
2018-03-14 14:42:29 +0000

    Extract colour inheritor to new mutator class
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

---
+++ models/mutator.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function(exports) {
+
+  function Mutator() {
+
+  }
+
+  Mutator.prototype.isMutation = function() {
+    return (Math.floor(Math.random() * 10) === 5)
+  };
+
+  exports.Mutator = Mutator;
+
+})(this);
