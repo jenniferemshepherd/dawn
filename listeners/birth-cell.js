3f81018490e49a46d0f59d1248a09f0f1f4190fe
rcvink <rrvink@gmail.com>
2018-03-14 13:07:22 +0000

    Extract listener for reproduction
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

---
+++ listeners/birth-cell.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function(exports) {
+
+  function BirthCell(cellFactory, cellRepository) {
+    this._cellFactory = cellFactory;
+    this._cellRepository = cellRepository;
+  }
+
+  BirthCell.prototype.action = function (event) {
+    var bodyA = event.pairs[0].bodyA;
+    var bodyB = event.pairs[0].bodyB;
+    var collisionTime = event.source.timing.timestamp;
+    if (this._reproductionShouldHappen(bodyA, bodyB, collisionTime)) {
+      this._updateParentsLastReproduction(bodyA, bodyB, collisionTime);
+      this._cellFactory.createFromParents(this._parent(bodyA), this._parent(bodyB), collisionTime);
+    };
+  };
+
+  BirthCell.prototype._reproductionShouldHappen = function (bodyA, bodyB, collisionTime) {
+    return this._isCompatible(bodyA, bodyB) && this._isFertile(bodyA, bodyB, collisionTime);
+  };
+
+  BirthCell.prototype._isCompatible = function (bodyA, bodyB) {
+    return !(this._isBoundary(bodyA) || this._isBoundary(bodyB));
+  };
+
+  BirthCell.prototype._isBoundary = function (body) {
+    return body.label === 'Rectangle Body';
+  };
+
+  BirthCell.prototype._isFertile = function (bodyA, bodyB, time) {
+    return this._parent(bodyA).isFertile(time) && this._parent(bodyB).isFertile(time);
+  };
+
+  BirthCell.prototype._parent = function (body) {
+    return this._cellRepository.findCellByBodyId(body.id);
+  };
+
+  BirthCell.prototype._updateParentsLastReproduction = function (bodyA, bodyB, collisionTime) {
+    this._parent(bodyA).updateLastReproduction(collisionTime);
+    this._parent(bodyB).updateLastReproduction(collisionTime);
+  };
+
+  exports.BirthCell = BirthCell;
+
+})(this);
