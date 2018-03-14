078e8713b2622f6682ae8e131eb2400d67b53f40
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 15:58:27 +0000

    add envelope play function

--- models/voice.js
+++ models/voice.js
@@ -6,24 +6,30 @@
     this._osc = new p5.Oscillator;
   }
 
-  Voice.prototype.start = function() {
-    this._osc.start()
-  };
-
   Voice.prototype.osc = function() {
     return this._osc
   };
 
+  Voice.prototype.start = function() {
+    this._osc.start()
+  };
+
   Voice.prototype.envelope = function() {
+    env = new p5.Env();
     var attackLevel = 1.0;
     var releaseLevel = 0;
     var attackTime = 0.1
     var decayTime = 0.2;
     var susPercent = 0.2;
     var releaseTime = 0.5;
-    env = new p5.Env();
     env.setADSR(attackTime, decayTime, susPercent, releaseTime);
     env.setRange(attackLevel, releaseLevel);
+    return env
+  };
+
+  Voice.prototype.playEnv = function() {
+    this._osc.amp(this.envelope())
+    this.envelope().play();
   };
 
 
