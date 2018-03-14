e7823fcc0381be06cda6c0dea32693f64c78e336
Ellie <elliewem@gmail.com>
2018-03-14 14:43:02 +0000

    Extract colour inheritor into new rgb-formatter
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

---
+++ models/rgb-formatter.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function(exports) {
+
+  function RgbFormatter() {
+
+  }
+
+  var TOP_RGB_VALUE = 255;
+
+  RgbFormatter.prototype.randomColour = function() {
+    return `rgb(${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)})`
+  };
+
+  RgbFormatter.prototype.albinoString = function() {
+    return `rgb(${TOP_RGB_VALUE}, ${TOP_RGB_VALUE}, ${TOP_RGB_VALUE})`
+  };
+
+  RgbFormatter.prototype.formatRgbString = function(parent) {
+    var rgbString = parent.body().render.fillStyle
+    var colourStringArr = rgbString.slice(4, rgbString.length-1).split(', ')
+    var intArray = []
+    colourStringArr.forEach(function(colourVal) { intArray.push(parseInt(colourVal))});
+    return intArray
+  };
+
+  RgbFormatter.prototype.convertToRgbString = function(array) {
+    var rgb = array.join(', ')
+    return `rgb(${rgb})`
+  };
+
+  exports.RgbFormatter = RgbFormatter
+
+})(this);
