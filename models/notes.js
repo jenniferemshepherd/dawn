e9fb2b353866f6abdda34c116a86cbcabb1b1b98
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 17:52:27 +0000

    add notes array to note class

---
+++ models/notes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function(exports) {
+
+  function Notes() {
+     this._noteArray = [830.609, 932.328, 1046.50, 1108.73, 1244.51, 1396.91, 1567.98, 1661.22]
+   };
+
+exports.Notes = Notes;
+
+})(this);
