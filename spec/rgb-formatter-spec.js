11aed0cd9e67051fbddf2bc36b389d8f8e3aab4d
Ellie <elliewem@gmail.com>
2018-03-14 15:03:31 +0000

    Amend format rgbstring test
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- spec/rgb-formatter-spec.js
+++ spec/rgb-formatter-spec.js
@@ -2,6 +2,14 @@
 
 describe("RgbFormatter", function() {
 
+  var render = {
+    fillStyle: 'rgb(100, 55, 66)'
+  }
+
+  var parent = {
+    body: function() { return render }
+  };
+
   beforeEach(function() {
     rgbFormatter = new RgbFormatter();
   });
@@ -10,6 +18,14 @@ describe("RgbFormatter", function() {
 
     it("returns a white rgb string", function() {
       expect(rgbFormatter.albinoString()).toEqual('rgb(255, 255, 255)')
+    });
+
+  });
+
+  describe("#formatRgbString", function() {
+
+    it("returns an array of numbers from an rgb string", function() {
+      expect(rgbFormatter.formatRgbString(parent)).toEqual([100, 55, 66])
     });
 
   });
