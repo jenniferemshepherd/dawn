40d5067fb85eae9abdd0619e1e91653cec4c0bc6
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 17:53:59 +0000

    add choose notes for random sample

--- models/notes.js
+++ models/notes.js
@@ -3,11 +3,15 @@
 (function(exports) {
 
   function Notes() {
-     this._noteArray = [830.609, 932.328, 1046.50, 1108.73, 1244.51, 1396.91, 1567.98, 1661.22]
-   };
+    this._noteArray = [830.609, 932.328, 1046.50, 1108.73, 1244.51, 1396.91, 1567.98, 1661.22]
+  };
 
-   Notes.prototype.array = function() {
-     return this._noteArray;
+  Notes.prototype.array = function() {
+    return this._noteArray;
+  };
+
+  Notes.prototype.choose = function() {
+    return this._noteArray[Math.floor(Math.random() * this._noteArray.length)];
   };
 
 exports.Notes = Notes;
