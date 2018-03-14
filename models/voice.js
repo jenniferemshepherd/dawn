e18994b74c76f6858818c9b2625b0e40050d4d5f
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 22:49:00 +0000

    remove Notes class and refactor to voice

--- models/voice.js
+++ models/voice.js
@@ -3,8 +3,7 @@
 (function(exports) {
 
   function Voice(notes) {
-    this._notes = notes
-    this._note = this._notes.choose()
+    this._noteArray = [830.609, 932.328, 1046.50, 1108.73, 1244.51, 1396.91, 1567.98, 1661.22]
     this._osc = new p5.Oscillator;
     this._attackLevel = 1.0;
     this._releaseLevel = 0;
@@ -28,8 +27,12 @@
     env.setRange(this._attackLevel, this._releaseLevel);
     this._osc.amp(env)
     this._osc.start()
-    this._osc.freq(this._note)
+    this._osc.freq(this.note())
     env.play();
+  };
+
+  Voice.prototype.note = function() {
+    return this._noteArray[Math.floor(Math.random() * this._noteArray.length)];
   };
 
 exports.Voice = Voice;
