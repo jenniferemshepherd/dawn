924a4497f23ff43c9df0e857421bbe3cb1ebdc65
rcvink <rrvink@gmail.com>
2018-03-14 16:45:15 +0000

    Reorganise instantiation
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- dawn.js
+++ dawn.js
@@ -1,46 +1,38 @@
-// instantiate our objects
-// var Common = Matter.Common;
+// instantiate decorators
 var decoratedEngine = new DecoratedEngine();
 var decoratedRender = new DecoratedRender();
+// var runner = new DecoratedRunner();
+var runner = Matter.Runner.create();
+
+// instantiate controllers
 var eventController = new EventController(decoratedEngine);
 var simulation = new Simulation(decoratedEngine, decoratedRender);
-var cellRepository = new CellRepository();
-var shapeInheritor = new ShapeInheritor();
+
+// instantiate inheritors and dependencies
 var positionInheritor = new PositionInheritor();
+var shapeInheritor = new ShapeInheritor();
 var rgbFormatter = new RgbFormatter();
 var mutator = new Mutator();
 var colourInheritor = new ColourInheritor(rgbFormatter, mutator);
+
+// instantiate repository and factory
+var cellRepository = new CellRepository();
 var cellFactory = new CellFactory(simulation, cellRepository, positionInheritor, shapeInheritor, colourInheritor);
+
+// instantiate listeners
 var animator = new Animator(cellRepository);
 var grow = new Grow(cellRepository);
 var cellFader = new CellFader(cellRepository);
 var killer = new Killer(cellRepository, simulation);
 var dawnTime = new DawnTime(decoratedEngine);
-
-
-// instantiate our listeners
 var birthCell = new BirthCell(cellFactory, cellRepository);
 
 // create render
 var render = decoratedRender.createRender(decoratedEngine.matterEngine());
 
-// create runner
-// var runner = new DecoratedRunner();
-var runner = Matter.Runner.create();
-
-// create some cells - now happens in jquery but only til refactor
-// var cell = cellFactory.createCircle();
-// cellFactory.createSquare();
-// cellFactory.createEquilateralTriangle();
-// cellFactory.createTrapezoid();
-
-// register our listeners
+// register listeners
 eventController.register('afterUpdate', animator);
 eventController.register('afterUpdate', grow);
 eventController.register('afterUpdate', killer);
 eventController.register('afterUpdate', cellFader);
 eventController.register('collisionStart', birthCell);
-
-// run things
-// simulation.setup();
-// simulation.run();
