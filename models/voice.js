b381b093b4f7dc648cd3a078a37ad3a47443901d
Ed Perkins <ed@edperkins.co.uk>
2018-03-14 15:11:48 +0000

    add envelope to voice

--- models/voice.js
+++ models/voice.js
@@ -14,6 +14,19 @@
     return this._osc
   };
 
+  Voice.prototype.envelope = function() {
+    var attackLevel = 1.0;
+    var releaseLevel = 0;
+    var attackTime = 0.1
+    var decayTime = 0.2;
+    var susPercent = 0.2;
+    var releaseTime = 0.5;
+    env = new p5.Env();
+    env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+    env.setRange(attackLevel, releaseLevel);
+  };
+
+
 exports.Voice = Voice;
 
 })(this);
