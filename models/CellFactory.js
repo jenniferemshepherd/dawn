bee2583278e9e8cc17fbf71c9444584baebf9aa0
rcvink <rrvink@gmail.com>
2018-03-14 13:08:26 +0000

    Remove extracted responsibilities
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- models/CellFactory.js
+++ models/CellFactory.js
@@ -84,42 +84,18 @@
   };
 
   CellFactory.prototype.createFromParents = function (parent1, parent2, time) {
-    if (this._areParentsFertile(parent1, parent2, time)) {
-      this._updateParentsLastReproduction(parent1, parent2, time);
-      var cell = new Cell(this._bodyModule.fromVertices(
-        this._positionInheritor.x(parent1, parent2),
-        this._positionInheritor.y(parent1, parent2),
-        this._shapeInheritor.childVertices(parent1, parent2),
-        { render: {fillStyle: this._colourInheritor.colourMixer(parent1, parent2) }}),
-                          new Gait(),
-                          new Age(time)
-                         );
-      this._cellRepository.add(cell);
-      this._simulation.addToWorld(cell);
-      return cell;
-    };
+    var cell = new Cell(this._bodyModule.fromVertices(
+      this._positionInheritor.x(parent1, parent2),
+      this._positionInheritor.y(parent1, parent2),
+      this._shapeInheritor.childVertices(parent1, parent2),
+      { render: {fillStyle: this._colourInheritor.colourMixer(parent1, parent2) }}),
+                        new Gait(),
+                        new Age(time)
+                       );
+    this._cellRepository.add(cell);
+    this._simulation.addToWorld(cell);
+    return cell;
   };
-
-  CellFactory.prototype._updateParentsLastReproduction = function (parent1, parent2, time) {
-    parent1.updateLastReproduction(time);
-    parent2.updateLastReproduction(time);
-  };
-
-  CellFactory.prototype.action = function (event) {
-    if (this._isCompatible(event)) {
-      var parent1 = this._cellRepository.findCellByBodyId(event.pairs[0].bodyA.id);
-      var parent2 = this._cellRepository.findCellByBodyId(event.pairs[0].bodyB.id);
-      this.createFromParents(parent1, parent2, event.source.timing.timestamp);
-    };
-  };
-
-  CellFactory.prototype._isCompatible = function (event) {
-    return (event.pairs[0].bodyA.label !== 'Rectangle Body' && event.pairs[0].bodyB.label !== 'Rectangle Body');
-  };
-
-  CellFactory.prototype._areParentsFertile = function (parent1, parent2, time) {
-    return parent1.isFertile(time) && parent2.isFertile(time);
-  }
 
   exports.CellFactory = CellFactory;
 
