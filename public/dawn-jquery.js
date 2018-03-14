942512b91e720215846c51530bfac5829c76b202
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-14 18:08:02 +0000

    refactor jquery file with functions
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- public/dawn-jquery.js
+++ public/dawn-jquery.js
@@ -1,46 +1,59 @@
 $( document ).ready(function() {
 
-  startSimulation(cellFactory, runner, decoratedEngine, simulation)
+  startSimulation(cellFactory, runner, decoratedEngine, simulation);
 
   $( "#start" ).click(function() {
-    if (decoratedEngine.matterEngine().timing.timestamp === 0) {
-      startSimulation(cellFactory, runner, decoratedEngine, simulation)
+    if (this.matterEngine().timing.timestamp === 0) {
+      startSimulation(cellFactory, runner, decoratedEngine, simulation);
     };
     });
 
   $( "#stop" ).click(function() {
-    Matter.Runner.stop(runner.matterRunner())
-    Matter.Engine.clear(decoratedEngine.matterEngine());
-    decoratedEngine.matterEngine().timing.timestamp = 0;
-    simulation.emptyWorld();
+    stopSimulation(runner, decoratedEngine, simulation);
     });
 
   $( "#pause" ).click(function() {
-    Matter.Runner.stop(runner.matterRunner())
+    stopRunner(runner);
     });
 
   $( "#unpause" ).click(function() {
-    Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
+    runRunner(runner, decoratedEngine)
     });
 
   $('#refresh').click(function() {
-    Matter.Runner.stop(runner.matterRunner())
-    Matter.Engine.clear(decoratedEngine.matterEngine());
-    decoratedEngine.matterEngine().timing.timestamp = 0;
-    simulation.emptyWorld();
-    startSimulation(cellFactory, runner, decoratedEngine, simulation)
+    stopSimulation(runner, decoratedEngine, simulation);
+    startSimulation(cellFactory, runner, decoratedEngine, simulation);
   });
 
   $('#wireframe').click(function() {
     decoratedRender.wireframeswitch();
   });
 
+  // functions for jQuery
 
   function startSimulation(cellFactory, runner, decoratedEngine, simulation) {
     cellFactory.createInitialShapes();
-    Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
+    runRunner(runner, decoratedEngine);
     simulation.setup();
     simulation.run();
   };
 
+  function stopSimulation(runner, decoratedEngine, simulation) {
+    stopRunner(runner);
+    clearEngine(decoratedEngine);
+    decoratedEngine.resetTimer();
+    simulation.emptyWorld();
+  };
+
+  function clearEngine(decoratedEngine) {
+    Matter.Engine.clear(decoratedEngine.matterEngine());
+  };
+
+  function stopRunner(runner) {
+    Matter.Runner.stop(runner.matterRunner());
+  };
+
+  function runRunner(runner, decoratedEngine) {
+    Matter.Runner.run(runner.matterRunner(), decoratedEngine.matterEngine());
+  };
 });
