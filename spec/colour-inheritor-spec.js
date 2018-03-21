3e7a56ce5d143840025349583b20508faf99dcad
rcvink <rrvink@gmail.com>
2018-03-21 10:55:01 +0000

    remove commented tests
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- spec/colour-inheritor-spec.js
+++ spec/colour-inheritor-spec.js
@@ -6,20 +6,6 @@ describe("ColourInheritor", function() {
 
   describe("#colourMixer", function() {
 
-    // it('returns a white rgb string if isMutation() is true twice', function() {
-    //   var colourInheritor = new ColourInheritor(parent, parent);
-    //   spyOn(colourInheritor, '_isMutation').and.returnValue(true);
-    //   expect(colourInheritor.colourMixer()).toEqual('rgb(255, 255, 255)')
-    // });
-    //
-    // it('returns a weighted value if isMutation() is false', function() {
-    //   var colourInheritor = new ColourInheritor(parent, parent);
-    //   spyOn(colourInheritor, '_isMutation').and.returnValue(false);
-    //   spyOn(colourInheritor, '_mixedColour')
-    //   colourInheritor.colourMixer(parent, parent)
-    //   expect(colourInheritor._mixedColour).toHaveBeenCalled()
-    // });
-
   });
 
 });
