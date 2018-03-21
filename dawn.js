2840daaca80952f783aa25d5e20d128dc8c9e020
rcvink <rrvink@gmail.com>
2018-03-21 11:07:58 +0000

    Rename instance of DecoratedRunner to match all other decorators
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- dawn.js
+++ dawn.js
@@ -1,3 +1,6 @@
+// insantiate decorators
+var decoratedRunner = new DecoratedRunner();
+
 // instantiate our objects
 var decoratedEngine = new DecoratedEngine();
 var decoratedRender = new DecoratedRender();
@@ -7,16 +10,16 @@ var cellRepository = new CellRepository();
 var cellFactory = new CellFactory(simulation, cellRepository, positionInheritor, shapeInheritor, colourInheritor);
 var dawnTime = new DawnTime(decoratedEngine);
 
-// instantiate our services
+// instantiate services
 var rgbFormatter = new RgbFormatter();
 var mutator = new Mutator();
 
-// instantiate our inheritors
+// instantiate inheritors
 var colourInheritor = new ColourInheritor(rgbFormatter, mutator);
 var positionInheritor = new PositionInheritor();
 var shapeInheritor = new ShapeInheritor();
 
-// instantiate our listeners
+// instantiate listeners
 var animateCells = new AnimateCells(cellRepository);
 var birthCell = new BirthCell(cellFactory, cellRepository);
 var fadeCells = new FadeCells(cellRepository);
@@ -27,9 +30,8 @@ var killCells = new KillCells(cellRepository, simulation);
 var render = decoratedRender.createRender(decoratedEngine.matterEngine());
 
 // create runner
-var runner = new DecoratedRunner();
 
-// register our listeners
+// register listeners
 eventController.register('afterUpdate', animateCells);
 eventController.register('collisionStart', birthCell);
 eventController.register('afterUpdate', fadeCells);
