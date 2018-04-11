bee5e59c84aecb9ac0e6288500d5210dcaa179cf
rcvink <rrvink@gmail.com>
2018-04-11 21:48:46 +0100

    Update RgbFormatter for testing
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- services/rgb-formatter.js
+++ services/rgb-formatter.js
@@ -1,34 +1,30 @@
 'use strict';
 
-(function(exports) {
+const TOP_RGB_VALUE = 255;
 
-  function RgbFormatter() {
+function RgbFormatter() {
 
-  }
+}
 
-  var TOP_RGB_VALUE = 255;
+RgbFormatter.prototype.randomColour = function() {
+  return `rgb(${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)})`;
+};
 
-  RgbFormatter.prototype.randomColour = function() {
-    return `rgb(${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)}, ${Math.floor(Math.random() * TOP_RGB_VALUE)})`;
-  };
+RgbFormatter.prototype.albinoString = function() {
+  return `rgb(${TOP_RGB_VALUE}, ${TOP_RGB_VALUE}, ${TOP_RGB_VALUE})`;
+};
 
-  RgbFormatter.prototype.albinoString = function() {
-    return `rgb(${TOP_RGB_VALUE}, ${TOP_RGB_VALUE}, ${TOP_RGB_VALUE})`;
-  };
+RgbFormatter.prototype.formatRgbString = function(parent) {
+  var rgbString = parent.body().render.fillStyle;
+  var colourStringArr = rgbString.slice(4, rgbString.length-1).split(', ');
+  var intArray = [];
+  colourStringArr.forEach(function(colourVal) { intArray.push(parseInt(colourVal))});
+  return intArray;
+};
 
-  RgbFormatter.prototype.formatRgbString = function(parent) {
-    var rgbString = parent.body().render.fillStyle;
-    var colourStringArr = rgbString.slice(4, rgbString.length-1).split(', ');
-    var intArray = [];
-    colourStringArr.forEach(function(colourVal) { intArray.push(parseInt(colourVal))});
-    return intArray;
-  };
+RgbFormatter.prototype.convertToRgbString = function(array) {
+  var rgb = array.join(', ');
+  return `rgb(${rgb})`;
+};
 
-  RgbFormatter.prototype.convertToRgbString = function(array) {
-    var rgb = array.join(', ');
-    return `rgb(${rgb})`;
-  };
-
-  exports.RgbFormatter = RgbFormatter;
-
-})(this);
+module.exports = RgbFormatter;
