60fdb20bbeaafb27a43c8baccbbf6846b8430534
rcvink <rrvink@gmail.com>
2018-04-11 21:43:18 +0100

    Update KillCells for testing
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- listeners/kill-cells.js
+++ listeners/kill-cells.js
@@ -1,25 +1,22 @@
 'use strict';
 
-(function(exports) {
+function KillCells(cellRepository, simulation, deathAge = 45000) {
+  this._cellRepository = cellRepository;
+  this._simulation = simulation;
+  this._deathAge = deathAge;
+}
 
-  function KillCells(cellRepository, simulation, deathAge = 45000) {
-    this._cellRepository = cellRepository;
-    this._simulation = simulation;
-    this._deathAge = deathAge;
-  }
+KillCells.prototype.action = function(event) {
+  this._cellRepository.store().forEach(function(cell) {
+    if (this._isCellDying(cell, event)) {
+      this._simulation.removeFromWorld(cell);
+      this._cellRepository.remove(cell);
+    };
+  }.bind(this));
+};
 
-  KillCells.prototype.action = function(event) {
-    this._cellRepository.store().forEach(function(cell) {
-      if (this._isCellDying(cell, event)) {
-        this._simulation.removeFromWorld(cell);
-        this._cellRepository.remove(cell);
-      };
-    }.bind(this));
-  };
+KillCells.prototype._isCellDying = function (cell, event) {
+  return cell.age().value(event.timestamp) > this._deathAge;
+};
 
-  KillCells.prototype._isCellDying = function (cell, event) {
-    return cell.age().value(event.timestamp) > this._deathAge;
-  };
-
-  exports.KillCells = KillCells;
-})(this);
+module.exports = KillCells;
--- spec/kill-cells-spec.js
+++ spec/kill-cells-spec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const KillCells = require('../listeners/kill-cells');
+
 describe("KillCells", function() {
   var killCells;
 
