367eda3b0d20518bc325686125a0b31287f1b707
rcvink <rrvink@gmail.com>
2018-04-11 21:46:34 +0100

    Update PositionInheritor for testing
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- inheritors/position-inheritor.js
+++ inheritors/position-inheritor.js
@@ -1,21 +1,17 @@
-(function(exports) {
+function PositionInheritor() {
 
-  function PositionInheritor() {
+}
 
-  }
+PositionInheritor.prototype.x = function (parent1, parent2) {
+  return this._mean(parent1.body().position.x, parent2.body().position.x);
+};
 
-  PositionInheritor.prototype.x = function (parent1, parent2) {
-    return this._mean(parent1.body().position.x, parent2.body().position.x);
-  };
+PositionInheritor.prototype.y = function (parent1, parent2) {
+  return this._mean(parent1.body().position.y, parent2.body().position.y);
+};
 
-  PositionInheritor.prototype.y = function (parent1, parent2) {
-    return this._mean(parent1.body().position.y, parent2.body().position.y);
-  };
+PositionInheritor.prototype._mean = function (number1, number2) {
+  return 0.5 * (number1 + number2);
+};
 
-  PositionInheritor.prototype._mean = function (number1, number2) {
-    return 0.5 * (number1 + number2);
-  };
-
-  exports.PositionInheritor = PositionInheritor;
-
-})(this);
+module.exports = PositionInheritor;
