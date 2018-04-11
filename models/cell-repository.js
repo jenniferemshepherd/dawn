4f9767efc36e854cfc0f816dc251fa71b952b45c
rcvink <rrvink@gmail.com>
2018-04-11 20:57:13 +0100

    Update CellRepository for testing
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- models/cell-repository.js
+++ models/cell-repository.js
@@ -1,29 +1,25 @@
 'use strict';
 
-(function(exports) {
+function CellRepository() {
+  this._store = [];
+}
 
-  function CellRepository() {
-    this._store = [];
-  }
+CellRepository.prototype.store = function() {
+  return this._store;
+};
 
-  CellRepository.prototype.store = function() {
-    return this._store;
-  };
+CellRepository.prototype.add = function(cell) {
+  this._store.push(cell);
+};
 
-  CellRepository.prototype.add = function(cell) {
-    this._store.push(cell);
-  };
+CellRepository.prototype.findCellByBodyId = function (id) {
+  return this._store.find(function(cell) {
+    return cell.body().id === id;
+  });
+};
 
-  CellRepository.prototype.findCellByBodyId = function (id) {
-    return this._store.find(function(cell) {
-      return cell.body().id === id;
-    });
-  };
+CellRepository.prototype.remove = function(cell) {
+  this._store.splice(this._store.indexOf(cell), 1);
+};
 
-  CellRepository.prototype.remove = function(cell) {
-    this._store.splice(this._store.indexOf(cell), 1);
-  };
-
-  exports.CellRepository = CellRepository;
-
-})(this);
+module.exports = CellRepository;
