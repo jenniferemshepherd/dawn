2f3a28150d6739e8e92eecad60f435dd33535319
rcvink <rrvink@gmail.com>
2018-04-11 21:50:56 +0100

    Update ShapeInheritor for testing
    co-authored by jenniferemshepherd <jennifer.elaine.mairead@gmail.com>, elliewem <elliewem@gmail.com>, rcvink <rrvink@gmail.com>, edpe <ed@edperkins.com>

--- inheritors/shape-inheritor.js
+++ inheritors/shape-inheritor.js
@@ -1,26 +1,24 @@
-(function(exports) {
+'use strict';
 
-  function ShapeInheritor(scaleFactor = 0.5) {
-    this._scaleFactor = scaleFactor;
-  }
+function ShapeInheritor(scaleFactor = 0.5) {
+  this._scaleFactor = scaleFactor;
+}
 
-  ShapeInheritor.prototype.childVertices = function (parent1, parent2) {
-    return this._scaleVertices(this._parentVertices(parent1, parent2));
-  };
+ShapeInheritor.prototype.childVertices = function (parent1, parent2) {
+  return this._scaleVertices(this._parentVertices(parent1, parent2));
+};
 
-  ShapeInheritor.prototype._scaleVertices = function (vertices) {
-    return vertices.map(vertex => {
-      return {
-        x: vertex.x * this._scaleFactor,
-        y: vertex.y * this._scaleFactor
-      };
-    });
-  };
+ShapeInheritor.prototype._scaleVertices = function (vertices) {
+  return vertices.map(vertex => {
+    return {
+      x: vertex.x * this._scaleFactor,
+      y: vertex.y * this._scaleFactor
+    };
+  });
+};
 
-  ShapeInheritor.prototype._parentVertices = function (parent1, parent2) {
-    return parent1.body().vertices.concat(parent2.body().vertices);
-  };
+ShapeInheritor.prototype._parentVertices = function (parent1, parent2) {
+  return parent1.body().vertices.concat(parent2.body().vertices);
+};
 
-  exports.ShapeInheritor = ShapeInheritor;
-
-})(this);
+module.exports = ShapeInheritor;
--- spec/shape-inheritor-spec.js
+++ spec/shape-inheritor-spec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ShapeInheritor = require('../inheritors/shape-inheritor');
+
 describe("ShapeInheritor", function() {
   var shapeInheritor;
 
